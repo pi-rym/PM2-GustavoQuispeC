3558688eab5c92e4ab7117f4e87d9b33b8233ba6
Gustavo <gusstavocta@hotmail.com>
2024-03-11 23:34:51 -0500

    implemtacion Axios

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,6 +1,34 @@
-const renderPeliculas=require('./renderCards');
+const renderPeliculas = require('./renderCards');
+const axios = require('axios');
 
-$.get('https://students-api.2.us-1.fl0.io/movies',(data)=>
-  data.forEach(renderPeliculas)
+// axios.get('https://students-api.up.railway.app/movies')
+// .then((response) => {
+//   console.log(response.data);
+//   response.data.forEach(pelicula => {
+//     renderPeliculas(pelicula);
+//   });
+// }).catch((err) => {
+//   console.log(err.response.data.message);
+// });
+
+const recuperarPeliculas = async () => {
+  try {
+    const peliculasData = await axios.get('https://students-api.up.railway.app/movies');
+    console.log(peliculasData);
+    peliculasData.data.forEach(pelicula => {
+      renderPeliculas(pelicula);
+    });
+    
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
   
-  )
+
+};
+recuperarPeliculas();
+
+
+// $.get('https://students-api.up.railway.app/movies',(data)=>
+//   data.forEach(renderPeliculas)
+  
+//   )
